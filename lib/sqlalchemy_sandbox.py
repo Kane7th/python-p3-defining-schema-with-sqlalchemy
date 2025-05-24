cce4fb55e29bbb7a319a523477c263f7742ef756
Kane Kabena <onekaneldn@gmail.com>
2025-05-24 15:00:09 +0300

    defining schema with sqlalchemy code-along completed

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,7 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    # This creates an SQLite database named 'students.db' in the current directory.
+    engine = create_engine('sqlite:///students.db') 
+    # This will create the database and the table if they do not exist.
+    Base.metadata.create_all(engine) 
+
